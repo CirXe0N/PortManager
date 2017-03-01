aa80a1bf4d3f45aed75f7bfe8c723cdb4a495374
CirXe0N <dw1ls@outlook.com>
2017-03-01 14:39:59 +0100

    Add login and user profile view

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.dock_overview, name='dock_overview'),
     url(r'^docks/(?P<dock_id>[\w-]+)/$', views.dock_details, name='dock_details'),
+    url(r'^authentication/$', views.authentication, name='authentication'),
+    url(r'^logout/$', views.do_logout_request, name='logout'),
+    url(r'^profile/$', views.user_profile, name='user_profile'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from web.models import Dock, CargoHazard
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, get_object_or_404, redirect
+from rest_framework.authtoken.models import Token
+
+from web.models import Dock, CargoHazard, DockSupervisor, Person, Ship
 
 
 def dock_overview(request):
@@ -24,6 +27,48 @@ def dock_details(request, dock_id):
 
     context = {
         'dock': get_object_or_404(Dock, dock_id=dock_id)
+    }
+
+    return render(request, template, context)
+
+
+def authentication(request):
+    template = 'web/authentication/authentication.html'
+
+    context = {
+        'is_failed_login': False,
+    }
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        supervisor = DockSupervisor.objects.filter(employee__person__user=user)
+
+        if len(supervisor) > 0:
+            login(request, user)
+            return redirect('dock_overview')
+        else:
+            context['is_failed_login'] = True
+
+    return render(request, template, context)
+
+
+def do_logout_request(request):
+    logout(request)
+    return redirect('dock_overview')
+
+
+def user_profile(request):
+    template = 'web/profile/user_profile.html'
+    person = get_object_or_404(Person, user=request.user)
+    ships = Ship.objects.filter(captain__person=person)
+    token = Token.objects.filter(user=request.user).first()
+
+    context = {
+        'person': person,
+        'ships': ships,
+        'token': token,
     }
 
     return render(request, template, context)
